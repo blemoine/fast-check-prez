703a5edf6e09adebac260136095ad5d89c8585f3
Benoit Lemoine <blemoine@xebia.fr>
2022-07-20 18:10:16 -0400

    init

---
+++ my-examples/index.spec.ts
@@ -0,0 +1,61 @@
+import fc from "fast-check";
+import { getExpeditionFees } from "./index";
+
+describe("getExpeditionFees", () => {
+  it("should return 0 if the price is 0", () => {
+    expect(getExpeditionFees(0, 10)).toStrictEqual(0);
+  });
+  it("should return 0 if the freeThreshold is 0", () => {
+    expect(getExpeditionFees(100, 0)).toStrictEqual(0);
+  });
+  it("should return the 0.9% of the price if it the fees are blow the threshold", () => {
+    expect(getExpeditionFees(1000, 10)).toStrictEqual(9);
+  });
+  it("should return 0 if it the fees are equal to the threshold", () => {
+    expect(getExpeditionFees(1000, 9)).toStrictEqual(0);
+  });
+  it("should return 0 if it the fees are above to the threshold", () => {
+    expect(getExpeditionFees(1000, 7)).toStrictEqual(0);
+  });
+
+  it("should return 0 if it the fees are equal to the threshold", () => {
+//    expect(getExpeditionFees(3000, 27)).toStrictEqual(0);
+  });
+
+  // --- Phase 2
+  it("should return 0 if the threshold is 0", () => {
+    fc.assert(
+      fc.property(fc.nat(), (price: number) => {
+        expect(getExpeditionFees(price, 0)).toStrictEqual(0);
+      })
+    );
+  });
+  it("should return something smaller than the threshold", () => {
+    fc.assert(
+      fc.property(fc.nat(), fc.nat(), (price: number, threshold: number) => {
+        expect(getExpeditionFees(price, threshold)).toBeLessThanOrEqual(
+          threshold
+        );
+      })
+    );
+  });
+});
+
+
+describe("property based", () => {
+  it("TODO", () => {
+    fc.assert(
+      fc.property(fc.nat(), (n) => {
+        expect(getExpeditionFees(n * 1000, n * 9)).toStrictEqual(0);
+      }),
+      { numRuns: 100000 }
+    );
+  });
+});
+
+/*
+it("TODO", () => {
+    // 3000 * 0.009 === 27
+    expect(getExpeditionFees(3000, 27)).toStrictEqual(0);
+  });
+ */
---
+++ my-examples/index.ts
@@ -0,0 +1,5 @@
+export function getExpeditionFees(price: number, freeThreshold: number): number {
+  const expeditionFeesRate = 0.009;
+  const expeditionFees = expeditionFeesRate * price;
+  return expeditionFees >= freeThreshold ? 0 : expeditionFees;
+}
